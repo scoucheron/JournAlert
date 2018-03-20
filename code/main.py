0047691e932388c34c5ebf76be35cdf11a0d327f
scoucheron <sverre.coucheron@gmail.com>
2018-03-20 12:31:42 +0100

    Added comments to all functions.

--- code/main.py
+++ code/main.py
@@ -1,14 +1,57 @@
 from utils import *
 
 def fetchJournal(patient_id, employee_id):
+	'''
+	Fetches the journal for a given patient. Only allowed if the given employee_id is an actual employee.
+    If the journal is fetched it has to be written to the log
+		Input:
+			@patient_id: the ID of the patient to fetch the journal from
+            @employee_id: The ID for the employee which
+		Output:
+			A journal from a patient (fetches the ID)
+	'''
+
     return 0
 
 def logEntry(patient_id, emlpoyee_id):
+    '''
+    Write to the log. The log consists of a @patient_id, a @employee_id and a timestamp when it was accessed.
+		Input:
+			@patient_id: the ID of the patient to fetch the journal from
+            @employee_id: The ID for the employee which
+        Output:
+            An entry in the log containing @patient_id, @employee_id and a timestamp
+    '''
+
     return 0
 
-def createEntry(patient_id, employee_id):
+
+def checkLog():
+    '''
+    Goes through the log and checks if it si a red, orange or green entry.
+        A green entry: the entry was ok (Nothing wrong)
+        An orange entry: the journal was accessed, but not within the scheduled time (Something might be wrong)
+        A red entry: The journal was accessed when there was no relation between the patient and employee (Something was wrong)
+
+        Input:
+            @
+        Output:
+            Prints the number of green, orange and red entries
+    '''
+
     return 0
 
+
+def returnAccessed(patient_id):
+    '''
+    Fetches a patients accessed journal and who has accessed them
+        Input:
+            @patient_id: the ID of a patient
+        Output:
+            Every access of the journal and who accessed it
+    '''
+
+    return 0
 
 
 def main():
--- code/utils.py
+++ code/utils.py
@@ -1,14 +1,85 @@
+
+def initializeDatabase():
+    '''
+    Initialization of the database (only have to do this once)
+        ### ADD SCHEMA DESCRIPTION
+    '''
+    return 0
+
 def createPatient():
+    '''
+    Create a patient with a journal (use help-function)
+        Input:
+            @patient_id
+            @name
+            @journal_id
+
+    '''
     return 0
 
 def createEmployee():
+    '''
+    Create a employee
+        Input:
+            @employee_id
+    '''
     return 0
 
 def deletePatient():
+    '''
+    Delete an appointment from the schedule
+        Input:
+            @patient_id
+    '''
     return 0
 
 def deleteEmployee():
+    '''
+    Delete an employee the schema
+        Input:
+            @employee_id
+    '''
     return 0
 
 def deleteAppointment():
+    '''
+    Delete an appointment from the schedule
+        Input:
+            @journal_id
+        Output:
+            Boolean -> If it was done or not (need an exception)
+    '''
+    return 0
+
+
+def createEntry(patient_id, employee_id, timeFrom, timeTo):
+    '''
+    Create an entry in the schedule
+        Input:
+            @patient_id: the ID of the patient to fetch the journal from
+            @employee_id: The ID for the employee which
+        Output:
+            An entry in the schedule containing a @patient_id, @employee_id, a time from and a time to
+    '''
+    return 0
+
+def printRedEntries():
+    '''
+    Prints all the red entries
+        Input: Log
+    '''
+    return 0
+
+def printOrangeEntries():
+    '''
+    Prints all the orange entries
+        Input: Log
+    '''
+    return 0
+
+def printGreenEntries():
+    '''
+    Prints all the green entries
+        Input: Log
+    '''
     return 0
