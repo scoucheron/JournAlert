041f333ccccb8fde3fe5f259bb8c7305fce0c8f5
scoucheron <sverre.coucheron@gmail.com>
2018-03-20 13:07:10 +0100

    Fixed printing of log data

--- code/utils.py
+++ code/utils.py
@@ -1,3 +1,4 @@
+import sqlite3
 
 def initializeDatabase():
     '''
@@ -6,6 +7,12 @@ def initializeDatabase():
     '''
     return 0
 
+def fillWithFakeData():
+    '''
+    Fills the database with fake data
+        Input:
+
+    '''
 def createPatient():
     '''
     Create a patient with a journal (use help-function)
@@ -63,23 +70,36 @@ def createEntry(patient_id, employee_id, timeFrom, timeTo):
     '''
     return 0
 
-def printRedEntries():
+def printLogEntry(color):
     '''
     Prints all the red entries
-        Input: Log
-    '''
-    return 0
+        Input: Color of wanted entries
 
-def printOrangeEntries():
+        Colors:
+            GREEN == 1
+            ORANGE == 2
+            RED == 3
     '''
-    Prints all the orange entries
-        Input: Log
-    '''
-    return 0
 
-def printGreenEntries():
-    '''
-    Prints all the green entries
-        Input: Log
-    '''
-    return 0
+    # Make sure the color is correct and that there will be no errors
+    if(color == 1):
+        symbol = ('GREEN',)
+    elif(color == 2):
+        symbol = ('ORANGE',)
+    elif(color == 3):
+        symbol = ('RED',)
+    else:
+        print("Input color was wrong. (Has to be green [1], orange [2] or red [3]")
+        return 0
+
+    # Create a connection to the log database
+    conn = sqlite3.connect('log.db')
+    c. = conn.cursor()
+
+    # Fetch all entries in the log with a given warning level (gree,orange,red) and print them
+    print(x.fetchall())
+    for row in c.execute('SELECT * FROM entries WHERE warning_level=?', symbol)
+        print(row)
+
+    # Close the connection
+    conn.close()
